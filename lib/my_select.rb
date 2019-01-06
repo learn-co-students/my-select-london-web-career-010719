438748f35f007066f522e815ddaee45a0b243457
parik panchal <parikspanchal@icloud.com>
2019-01-06 23:12:14 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  returned_array = []
+  while i < collection.length
+    if yield(collection[i]) == true
+      returned_array.push(collection[i])
+    end
+    i += 1
+  end
+  returned_array
 end
